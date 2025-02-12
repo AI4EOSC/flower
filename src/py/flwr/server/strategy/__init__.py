0416954a617bd396754022e64708636c40b6dd62
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2025-02-12 10:25:15 +0100

    Add metric privacy from the server side with fixed clipping

--- src/py/flwr/server/strategy/__init__.py
+++ src/py/flwr/server/strategy/__init__.py
@@ -56,6 +56,7 @@ __all__ = [
     "DifferentialPrivacyClientSideFixedClipping",
     "DifferentialPrivacyServerSideAdaptiveClipping",
     "DifferentialPrivacyServerSideFixedClipping",
+    "MetricDifferentialPrivacyServerSideFixedClipping",
     "FaultTolerantFedAvg",
     "FedAdagrad",
     "FedAdam",
