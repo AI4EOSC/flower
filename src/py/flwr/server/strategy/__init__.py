9b73963ffab8b255c3dfa3707335c9dfa098c1df
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2025-02-13 09:07:55 +0100

    Add support for metric DP when using the tokens

--- src/py/flwr/server/strategy/__init__.py
+++ src/py/flwr/server/strategy/__init__.py
@@ -28,6 +28,9 @@ from .dp_fixed_clipping import (
 from .dp_fixed_clipping import (
     DifferentialPrivacyServerSideFixedClipping as DifferentialPrivacyServerSideFixedClipping,
 )
+from .metricdp_fixed_clipping import (
+    MetricDifferentialPrivacyServerSideFixedClipping as MetricDifferentialPrivacyServerSideFixedClipping,
+)
 from .dpfedavg_adaptive import DPFedAvgAdaptive as DPFedAvgAdaptive
 from .dpfedavg_fixed import DPFedAvgFixed as DPFedAvgFixed
 from .fault_tolerant_fedavg import FaultTolerantFedAvg as FaultTolerantFedAvg
@@ -56,6 +59,7 @@ __all__ = [
     "DifferentialPrivacyClientSideFixedClipping",
     "DifferentialPrivacyServerSideAdaptiveClipping",
     "DifferentialPrivacyServerSideFixedClipping",
+    "MetricDifferentialPrivacyServerSideFixedClipping",
     "FaultTolerantFedAvg",
     "FedAdagrad",
     "FedAdam",
