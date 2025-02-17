39d1008d293605483a0d39afa610dc67e7d8344c
Judith Sáinz-Pardo Díaz <sainzpardo@ifca.unican.es>
2025-02-17 12:39:21 +0100

    Update grpc.py

--- src/py/flwr/common/grpc.py
+++ src/py/flwr/common/grpc.py
@@ -48,7 +48,7 @@ def create_channel(
     insecure: bool,
     root_certificates: Optional[bytes] = None,
     max_message_length: int = GRPC_MAX_MESSAGE_LENGTH,
-    call_credentials: Optional[grpc.ChannelCredentials] = None
+    call_credentials: Optional[grpc.ChannelCredentials] = None,
     interceptors: Optional[Sequence[grpc.UnaryUnaryClientInterceptor]] = None,
 ) -> grpc.Channel:
     """Create a gRPC channel, either secure or insecure."""
